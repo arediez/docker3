0a64be66d5f1af39a9c6051c06ba5c0e5f07fd2f
arediez <61407218+arediez@users.noreply.github.com>
2020-11-17 11:16:38 +0700

    edit app.js

--- src/App.js
+++ src/App.js
@@ -8,14 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>AWS Elastic Beanstalk with Travis CI as CI and Docker as Image Builder</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          aredie was here
-        </a>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">aredie was here</a>
       </header>
     </div>
   );
