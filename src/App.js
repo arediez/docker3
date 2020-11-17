af8e57a8e2e21927045a9ab615c9cad0321f584e
arediez <61407218+arediez@users.noreply.github.com>
2020-11-17 11:05:51 +0700

    edit some text on app.js

--- src/App.js
+++ src/App.js
@@ -7,14 +7,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Bye there!</p>
+        <p>AWS Elastic Beanstalk with Travis CI as CI and Docker as Image Builder</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          aredie was here
         </a>
       </header>
     </div>
